c2bc062a43d450e346256a40d998d13b36ec0c3f
supme <supmea@gmail.com>
2018-02-26 11:40:47 +0300

    pipe send

--- sender.go
+++ sender.go
@@ -12,80 +12,72 @@ type Config struct {
 	Port       int
 	Stream     int
 	MapIP      map[string]string
-	SMTPserver SMTPserver
+	SMTPserver *SMTPserver
 }
 
 // Pipe email pipe for send email
 type Pipe struct {
-	email  chan<- Email
+	email  chan Email
 	config []Config
-	finish chan struct{}
-	stoped bool
-	sync.Mutex
-}
-
-type sender struct {
-	conf   Config
-	backet chan struct{}
+	finish chan bool
 }
 
 // NewPipe return new stream sender pipe
-func NewPipe(conf ...Config) *Pipe {
-	pipe := &Pipe{stoped: true}
+func NewPipe(conf ...Config) Pipe {
+	pipe := Pipe{}
+	pipe.finish = make(chan bool, 1)
 	for _, c := range conf {
 		pipe.config = append(pipe.config, c)
 	}
+	pipe.email = make(chan Email, len(conf))
 	return pipe
 }
 
 // Start stream sender
 func (pipe *Pipe) Start() {
-	for _, c := range pipe.config {
-		s := new(sender)
-		s.backet = make(chan struct{}, c.Stream)
-		s.conf = c
-		go func(s *sender) {
-			for e := range pipe.email {
-				s.backet <- struct{}{}
-				func(e *Email) {
-					conn := new(Connect)
-					conn.SetHostName(s.conf.Hostname)
-					conn.SetSMTPport(s.conf.Port)
-					conn.SetIface(s.conf.Iface)
-					conn.mapIP = s.conf.MapIP
-					e.Send(conn, &s.conf.SMTPserver)
-					_ = <-s.backet
-				}(&e)
-			}
-			close(s.backet)
-			pipe.finish <- struct{}{}
-		}(s)
-	}
-	pipe.Lock()
-	pipe.stoped = false
-	pipe.Unlock()
+	go func() {
+		wg := &sync.WaitGroup{}
+
+		for i := range pipe.config {
+			wg.Add(1)
+			go func(conf *Config) {
+				backet := make(chan struct{}, conf.Stream)
+				for email := range pipe.email {
+					backet <- struct{}{}
+					go func(e Email) {
+						conn := new(Connect)
+						conn.SetHostName(conf.Hostname)
+						conn.SetSMTPport(conf.Port)
+						conn.SetIface(conf.Iface)
+						conn.mapIP = conf.MapIP
+						e.Send(conn, conf.SMTPserver)
+						_ = <-backet
+					}(email)
+				}
+				close(backet)
+				wg.Done()
+			}(&pipe.config[i])
+		}
+
+		wg.Wait()
+		close(pipe.finish)
+	}()
 }
 
 // Send add email to stream
-func (pipe *Pipe) Send(email Email) error {
-	pipe.Lock()
-	defer pipe.Unlock()
-	if !pipe.stoped {
-		return errors.New("email streaming pipe stopped")
-	}
+func (pipe *Pipe) Send(email Email) {
+	defer func(e *Email) {
+		if err := recover(); err != nil {
+			e.ResultFunc(Result{ID: e.ID, Err: errors.New("421 email streaming pipe stopped")})
+		}
+	}(&email)
 	pipe.email <- email
-	return nil
 }
 
 // Stop stream sender
 func (pipe *Pipe) Stop() {
-	pipe.Lock()
-	pipe.stoped = true
 	close(pipe.email)
-	pipe.Unlock()
-	for i := 0; i < len(pipe.config); i++ {
-		_ = <-pipe.finish
-	}
+	_ = <-pipe.finish
 }
 
 // NewEmailPipe return new chanel for stream send

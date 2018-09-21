fe17598b14d8fb5df65ec934411ed138a1f19f04
supme <supmea@gmail.com>
2018-09-21 10:22:10 +0300

    all smtp servers different...

--- email.go
+++ email.go
@@ -114,11 +114,8 @@ func (e *Email) send(auth smtp.Auth, client *smtp.Client) error {
 	if err != nil {
 		return err
 	}
+	defer w.Close()
 	err = e.WriteCloser(w)
-	if err != nil {
-		return err
-	}
-	w.Close()
 	if err != nil {
 		return err
 	}
